cf023801c6535d6e126dee31bac104d3481a9d50
mangalsingh123t <121483110+mangalsingh123t@users.noreply.github.com>
2024-08-09 21:46:29 +0530

    Setup for url-shortner app

---
+++ index.js
@@ -0,0 +1,21 @@
+
+    const express = require("express")
+    const app = express()
+    const router = require('./routes/Url')
+    const staticRouter = require('./routes/StaticRouter')
+    const path = require('path')
+    const PORT = 9090
+    const connectMongodb = require('./Connection')
+
+    app.set('view engine', 'ejs');
+    app.set('views',path.resolve("./views"))
+
+    app.use(express.json());
+    app.use(express.urlencoded({extended:false}))
+
+    connectMongodb("mongodb://localhost:27017/short-url")
+    app.use("/url",router)
+
+    app.use("/",staticRouter)
+
+    app.listen(PORT, () => console.log(`Server is Started at ${PORT}`))
